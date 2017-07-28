59494d5959e3a92e4b6f2a2d9e3b1f009d7f4b6f
pt102560 <ricardo.silva@coriant.com>
2017-07-28 16:32:15 +0100

    Fix webview rendering.

--- app/src/main/java/com/zenhub/repodetails/RepoActivity.kt
+++ app/src/main/java/com/zenhub/repodetails/RepoActivity.kt
@@ -129,7 +129,12 @@ class OnRepoDetailsResponse(val parent: ViewGroup) : Callback<RepositoryDetails>
 
 class OnReadmeResponse(val parent: ViewGroup) : Callback<ResponseBody> {
 
-    val styleSheet = "<style>body{color: #ffffff; background-color: #303030;} a {color: #3f51b5;}</style>"
+    val styleSheet = """
+    <style>
+        body {color: #ffffff; background-color: #303030;}
+        a {color: #3f51b5;}
+        pre {overflow: auto; width: 95%;}
+    </style>"""
 
     override fun onFailure(call: Call<ResponseBody>?, t: Throwable?) {
         Log.d(LOGTAG, "Failed: ${t.toString()}")
@@ -140,7 +145,7 @@ class OnReadmeResponse(val parent: ViewGroup) : Callback<ResponseBody> {
         //TODO Deal with non 200OK response
         val webView = parent.findViewById<WebView>(R.id.readme_webview)
         val content = styleSheet + response.body()?.string()
-        webView.loadData(content, "text/html", null)
+        webView.loadDataWithBaseURL("https://github.com", content, "text/html", "UTF-8", null)
 
         val refreshLayout = parent.findViewById<SwipeRefreshLayout>(R.id.readme_swiperefresh)
         refreshLayout.isRefreshing = false
