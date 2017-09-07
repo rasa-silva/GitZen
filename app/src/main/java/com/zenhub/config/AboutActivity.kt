0d027e88e8284070346bf0e66baeb4e2b91b8d43
pt102560 <ricardo.silva@coriant.com>
2017-09-07 17:11:41 +0100

    Cleaner links on About screen.

--- app/src/main/java/com/zenhub/config/AboutActivity.kt
+++ app/src/main/java/com/zenhub/config/AboutActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.method.LinkMovementMethod
 import com.zenhub.BuildConfig
 import com.zenhub.R
 import kotlinx.android.synthetic.main.activity_about.*
@@ -22,5 +23,14 @@ class AboutActivity : AppCompatActivity() {
             intent.data = Uri.parse("market://details?id=${BuildConfig.APPLICATION_ID}")
             startActivity(intent)
         }
+
+        support_lib.movementMethod = LinkMovementMethod.getInstance()
+        kotlin.movementMethod = LinkMovementMethod.getInstance()
+        kotlin_coroutines.movementMethod = LinkMovementMethod.getInstance()
+        retrofit.movementMethod = LinkMovementMethod.getInstance()
+        picasso.movementMethod = LinkMovementMethod.getInstance()
+        highlightjs.movementMethod = LinkMovementMethod.getInstance()
+        kotlin_coroutines_retrofit.movementMethod = LinkMovementMethod.getInstance()
+
     }
 }
