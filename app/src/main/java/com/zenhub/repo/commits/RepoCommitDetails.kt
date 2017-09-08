ecb2a625f6da6d9322f5ed7a4260bd916170084b
pt102560 <ricardo.silva@coriant.com>
2017-09-08 10:37:19 +0100

    Misc tweaks.

--- app/src/main/java/com/zenhub/repo/commits/RepoCommitDetails.kt
+++ app/src/main/java/com/zenhub/repo/commits/RepoCommitDetails.kt
@@ -2,7 +2,6 @@ package com.zenhub.repo.commits
 
 import android.os.Bundle
 import android.support.v4.app.NavUtils
-import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
@@ -26,6 +25,8 @@ import com.zenhub.github.CommitFile
 import com.zenhub.github.gitHubService
 import com.zenhub.showErrorOnSnackbar
 import com.zenhub.showExceptionOnSnackbar
+import kotlinx.android.synthetic.main.progress_bar_overlay.*
+import kotlinx.android.synthetic.main.repo_commit_activity.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import ru.gildor.coroutines.retrofit.Result
@@ -44,14 +45,14 @@ class RepoCommitDetails : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = repoName
 
-        val recyclerViewAdapter = CommitFilesRecyclerViewAdapter()
         findViewById<RecyclerView>(R.id.files).let {
             val layoutManager = LinearLayoutManager(it.context)
             it.layoutManager = layoutManager
-            it.adapter = recyclerViewAdapter
+            it.adapter = CommitFilesRecyclerViewAdapter()
             it.addItemDecoration(DividerItemDecoration(it.context, layoutManager.orientation))
         }
 
+        swiperefresh.setOnRefreshListener { requestDataRefresh() }
         requestDataRefresh()
     }
 
@@ -68,7 +69,8 @@ class RepoCommitDetails : AppCompatActivity() {
     private fun requestDataRefresh() {
         launch(UI) {
             Log.d(Application.LOGTAG, "Refreshing commit details...")
-            val refreshLayout = findViewById<SwipeRefreshLayout>(R.id.swiperefresh)
+            progress_overlay.visibility = View.VISIBLE
+
             val result = gitHubService.commit(repoName, commitSha).awaitResult()
             when (result) {
                 is Result.Ok -> {
@@ -82,20 +84,21 @@ class RepoCommitDetails : AppCompatActivity() {
                                 .append(firstAndOtherLines[1], RelativeSizeSpan(0.75f), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
                     }
 
-                    refreshLayout.findViewById<TextView>(R.id.commit_message).text = styledCommitMsg
+                    swiperefresh.findViewById<TextView>(R.id.commit_message).text = styledCommitMsg
 
-                    val commit_details_stats = refreshLayout.findViewById<TextView>(R.id.commit_details_stats)
+                    val commit_details_stats = swiperefresh.findViewById<TextView>(R.id.commit_details_stats)
                     val statsText = commit_details_stats.resources.getString(R.string.commit_details_stats, response.commit.committer.name, response.files.size)
                     commit_details_stats.text = statsText
 
-                    val adapter = refreshLayout.findViewById<RecyclerView>(R.id.files).adapter as CommitFilesRecyclerViewAdapter
+                    val adapter = swiperefresh.findViewById<RecyclerView>(R.id.files).adapter as CommitFilesRecyclerViewAdapter
                     adapter.updateDataSet(response.files)
                 }
-                is Result.Error -> showErrorOnSnackbar(refreshLayout, result.response.message())
-                is Result.Exception -> showExceptionOnSnackbar(refreshLayout, result.exception)
+                is Result.Error -> showErrorOnSnackbar(swiperefresh, result.response.message())
+                is Result.Exception -> showExceptionOnSnackbar(swiperefresh, result.exception)
             }
 
-            refreshLayout.isRefreshing = false
+            progress_overlay.visibility = View.GONE
+            swiperefresh.isRefreshing = false
         }
     }
 }
@@ -117,7 +120,7 @@ class CommitFilesRecyclerViewAdapter : RecyclerView.Adapter<CommitFilesRecyclerV
 
         val patchView = view.findViewById<HighlightJsView>(R.id.patch)
         with(patchView) {
-            theme = Theme.TOMORROW_NIGHT_EIGHTIES
+            theme = Theme.GRUVBOX_DARK
             highlightLanguage = Language.DIFF
             setZoomSupportEnabled(true)
             setBackgroundColor(android.graphics.Color.TRANSPARENT)
